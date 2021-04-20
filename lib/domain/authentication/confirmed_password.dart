8a7bd07ff6461292c733b8156cf4d0737d82c000
Tudor <tudor.batica@gmail.com>
2021-04-20 20:16:09 +0300

    add `effectivedart` linter&repair codestyle issues

--- lib/domain/authentication/confirmed_password.dart
+++ lib/domain/authentication/confirmed_password.dart
@@ -1,13 +1,17 @@
 import 'package:formz/formz.dart';
 
+// ignore: public_member_api_docs
 enum ConfirmedPasswordValidationError { invalid }
 
+// ignore: public_member_api_docs
 class ConfirmedPassword
     extends FormzInput<String, ConfirmedPasswordValidationError> {
   const ConfirmedPassword.pure({this.password = ''}) : super.pure('');
+  // ignore: public_member_api_docs
   const ConfirmedPassword.dirty({required this.password, String value = ''})
       : super.dirty(value);
 
+  // ignore: public_member_api_docs
   final String password;
 
   @override
--- lib/presentation/core/root_widget.dart
+++ lib/presentation/core/root_widget.dart
@@ -1,10 +1,11 @@
-import 'package:challengesapp/application/core/app_cubit.dart';
-import 'package:challengesapp/presentation/authentication/sign_in_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../../application/core/app_cubit.dart';
 import '../../dependencies_configuration.dart';
+import '../authentication/sign_in_page.dart';
 
+/// The app widget.
 class RootWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
