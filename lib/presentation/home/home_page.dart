25a8c4599435bd8ea6fe897940fdabdb395d12ba
Tudor <tudor.batica@gmail.com>
2021-04-21 17:54:42 +0300

    remove old home nav method

--- lib/presentation/home/home_page.dart
+++ lib/presentation/home/home_page.dart
@@ -5,11 +5,6 @@ class HomePage extends StatelessWidget {
   /// Constructor
   const HomePage({Key? key}) : super(key: key);
 
-  /// Returns a `MaterialPageRoute` with a `HomePage` builder
-  static Route materialPageRoute() {
-    return MaterialPageRoute<void>(builder: (_) => const HomePage());
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
