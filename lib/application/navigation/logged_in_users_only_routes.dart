8bd89e6918f5ffd7cc478e5ac5e994918cf29515
Tudor <tudor.batica@gmail.com>
2021-05-27 20:59:45 +0300

    add a list of logged in users only routes

---
+++ lib/application/navigation/logged_in_users_only_routes.dart
@@ -0,0 +1,4 @@
+import 'package:challengesapp/application/navigation/route_names.dart';
+
+/// Defines which routes are accessible only to logged in users.
+List<String> loggedInUsersOnlyRoutes = [profileRoute, hostChallengeRoute];
