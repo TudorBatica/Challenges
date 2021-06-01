b97ef12079e78c86c4389ce020741da635917d0e
Tudor <tudor.batica@gmail.com>
2021-06-01 23:00:38 +0300

    update profile page

--- lib/presentation/profile/profile_page.dart
+++ lib/presentation/profile/profile_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:loading_indicator/loading_indicator.dart';
 
 import '../../application/common/app_cubit.dart';
 
@@ -12,13 +13,28 @@ class ProfilePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: BlocBuilder<AppCubit, AppState>(
+        buildWhen: (previous, current) => previous.user != current.user,
         builder: (context, state) {
-          return Column(
-            children: [
-              SelectableText(state.user.identity.email ?? "no email"),
-              Text(state.user.profile?.name ?? 'no name'),
-            ],
-          );
+          return state.user.identity.isAnonymous || state.user.profile == null
+              ? LoadingIndicator(indicatorType: Indicator.orbit)
+              : Center(
+                  child: Column(
+                    children: [
+                      SelectableText(
+                        state.user.profile?.name ?? 'no name',
+                        style: Theme.of(context).textTheme.headline3,
+                      ),
+                      SizedBox(height: 45.0),
+                      SelectableText('Email and Unique ID',
+                          style: Theme.of(context).textTheme.headline5),
+                      SizedBox(height: 15.0),
+                      SelectableText(state.user.identity.email.toString(),
+                          style: Theme.of(context).textTheme.headline6),
+                      SelectableText('ID: ${state.user.identity.id}',
+                          style: Theme.of(context).textTheme.headline6),
+                    ],
+                  ),
+                );
         },
       ),
     );
