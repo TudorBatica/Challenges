d31b1de9f886e380d3c963b0d4568681eafe96a9
Tudor <tudor.batica@gmail.com>
2021-06-01 20:51:38 +0300

    fix messages retrieval

--- lib/infrastructure/chat/firestore_chat_repository.dart
+++ lib/infrastructure/chat/firestore_chat_repository.dart
@@ -16,20 +16,20 @@ class FirestoreChatRepository implements ChatRepository {
   FirestoreChatRepository(this._serializer, this._firebaseFirestore);
 
   @override
-  Stream<List<Message>> listenToConversation(String teamId) async* {
-    await for (var conversationSnapshot in _firebaseFirestore
+  Stream<List<Message>> listenToConversation(String teamId) {
+    return _firebaseFirestore
         .collection(chatCollectionPath(teamId))
-        .snapshots()) {
-      yield conversationSnapshot.docs
-          .map((doc) => Message.fromJson(doc.data()))
-          .toList();
-    }
+        .orderBy('createdAt')
+        .limit(25)
+        .snapshots()
+        .asyncMap((snapshot) => snapshot.docs
+            .map((doc) => _serializer.fromJson(doc.data()))
+            .toList());
   }
 
   @override
   Future<void> sendMessage(String teamId, Message message) async {
     var messageMap = _serializer.toJson(message);
-    messageMap.update('createdAt', (value) => FieldValue.serverTimestamp());
     await _firebaseFirestore
         .collection(chatCollectionPath(teamId))
         .add(messageMap);
